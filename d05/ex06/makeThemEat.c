a417eade746d5d36f8a64a337550539a2015e1f5
moonsonu <moonk90@gmail.com>
2018-09-26 21:05:17 -0700

    d05

--- d05/ex06/makeThemEat.c
+++ d05/ex06/makeThemEat.c
@@ -6,7 +6,7 @@
 /*   By: ksonu <ksonu@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 17:17:48 by ksonu             #+#    #+#             */
-/*   Updated: 2018/09/26 19:50:50 by ksonu            ###   ########.fr       */
+/*   Updated: 2018/09/26 21:05:05 by ksonu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,27 @@
 
 int		available(struct s_people **people, int nbTable, int totalTime)
 {
-	int  tmp = 0;
-	if (nbTable == 0 || tmp == totalTime)
-		return (0);
+	int		tmp = totalTime;
+	if (nbTable == 0 && totalTime >= 0)
+		return (1);
 	for (int i = 0; i < nbTable; i++)
 	{
-		tmp += people[i]->time;
-		available(people, nbTable - 1, tmp);
-		tmp = 0;
+		printf("before %d\n", tmp);
+		tmp -= people[i]->time;
+		available(people, nbTable - 1, totalTime - tmp);
 	}
-	return (tmp);
+	printf("after total%d\n", totalTime);
+	printf("after tmp%d\n", tmp);
+	return (totalTime - tmp);
 }
 
 int		isPossible(struct s_people **people, int nbTable, int totalTime)
 {
-	if (!available(people, nbTable, totalTime))
+	if (available(people, nbTable, totalTime * nbTable) >= 0)
+	{
+		printf("result %d\n", available(people, nbTable, totalTime * nbTable));
 		return (1);
+	}
+		printf("result %d\n", available(people, nbTable, totalTime * nbTable));
 	return (0);
 }
